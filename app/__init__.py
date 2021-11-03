32664c7c30c283cb81a1e9b1d632a60ce8af7753
tryingtolearn11 <damiensingh17@gmail.com>
2021-11-02 21:46:24 -0400

    login form & requirements.txt & secret_key

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# TODO: This secret key is temporary
+# It will need its own file in the future
+# that will not get pushed to the repo.
+# FOR NOW, it remains here and just ignore it
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app 
-from flask import render_template
+from flask import render_template, url_for, redirect
+from app.forms import LoginForm
 
 ''' All Handlers go here. For example: 
     Login function -> login(),
@@ -19,7 +20,8 @@ def index():
 # Login Page
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 
 
