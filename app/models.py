6b5db4982b5bcab026883faddcd788e529be6de1
tryingtolearn11 <damiensingh17@gmail.com>
2021-11-15 14:30:21 -0500

    models

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    
+    # Print objects of this class
+    def __repr__(self):
+        return '<User {}>\n Email {}'.format(self.username, self.email)
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
