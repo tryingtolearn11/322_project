51bb4e32fca38a835af46c332b32f7da3ecadd40
Josh <joshmiranda.exe@gmail.com>
2021-11-23 20:43:43 -0500

    removed register

--- app/routes.py
+++ app/routes.py
@@ -31,11 +31,6 @@ def account():
     userPackage = userInfo()
     return render_template('account.html', packages=userPackage)
 
-# Register Page
-@app.route("/register")
-def register():
-    return render_template('register.html')
-
 # Course Page
 @app.route("/course")
 def course():
