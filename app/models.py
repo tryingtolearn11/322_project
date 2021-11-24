a5b32342c6beaaeec9c2305d0bc9526a4d12843a
tryingtolearn11 <damiensingh17@gmail.com>
2021-11-24 10:41:20 -0500

    password hashing

--- app/models.py
+++ app/models.py
@@ -1,19 +1,27 @@
 from app import login
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
 
 class User(UserMixin):
 
-    def __init__(self, username, password_hash):
+    def __init__(self, username, email):
         self.username = username
-        self.email = ""
-        self.password_hash = password_hash
+        self.email = email
+        self.password_hash = None
         self.posts = []
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
     def __repr__(self):
-        return '<User> {}>'.format(self.username)
+        return '<User {}, Email {}>'.format(self.username, self.email)
 
     
     @property
