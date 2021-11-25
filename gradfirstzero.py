a6f793a13f021580be72582b1186c4944024c99e
tryingtolearn11 <damiensingh17@gmail.com>
2021-11-25 11:32:54 -0500

    shell context

--- gradfirstzero.py
+++ gradfirstzero.py
@@ -1,3 +1,9 @@
 from app import app
+from app.models import User, registered_users_table
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'User' : User, 'registered_users_table': registered_users_table}
+
 
 
